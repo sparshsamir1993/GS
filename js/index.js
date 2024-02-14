ede6e207a549fa9c55ad66ce9c35505c7c3a2f64
sparshsamir <sparshsamir1993@gmail.com>
2024-02-13 23:15:23 -0500

    Fixed header scroll issue

--- js/index.js
+++ js/index.js
@@ -8,4 +8,15 @@ $(document).ready(function () {
         $('#nav-icon').toggleClass('open');
         $('.nav-wrapper').toggleClass('open');
     });
+
+    $('.nav-links a, .header-block a').click(function (e) {
+        e.preventDefault();
+        var sectionId = $(this).data('section');
+        var section = $('#' + sectionId);
+        if (section.length) {
+            $('html, body').animate({
+                scrollTop: section.offset().top
+            }, 800);
+        }
+    });
 });
